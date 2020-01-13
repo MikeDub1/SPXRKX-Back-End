2ce4479e3a02a311da123eb237b5234538665130
AHippoWhoPrograms <mjduboc@gmail.com>
2020-01-13 15:04:26 -0500

    User class has been defined.

--- SPRX/src/main/java/com/indies/sprx/User.java
+++ SPRX/src/main/java/com/indies/sprx/User.java
@@ -5,10 +5,72 @@
  */
 package com.indies.sprx;
 
+
 /**
  *
- * @author mians
+ * @author mians, dubocm
  */
-public class User {
-    
+public abstract class User 
+{
+    private String Fname;
+    private String Lname;
+    private String email;
+    private double xloc;
+    private double yloc;
+    private String username;
+    private String password;
+    private int Age;
+
+
+    public User(String Fname, String Lname, String email, double xloc, double yloc, String username, String password, int Age)
+    {
+        this.Fname = Fname;
+        this.Lname = Lname;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.Age = Age;
+        this.xloc = xloc;
+        this.yloc = yloc;
+
+    }
+
+    public String getFname()
+    {
+        return Fname;
+    }
+
+    public String getLname()
+    {
+        return Lname;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public double[] getCurrLoc()
+    {
+        double[] currLoc = new double[2];
+        currLoc[0] = xloc;
+        currLoc[1] = yloc;
+        return currLoc;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public int getAge()
+    {
+        return Age;
+    }
+
 }
