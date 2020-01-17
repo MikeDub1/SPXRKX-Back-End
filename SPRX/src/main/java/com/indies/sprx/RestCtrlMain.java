723b479ed8ec70bbeba6237d7d4b323793131cec
AHippoWhoPrograms <mjduboc@gmail.com>
2020-01-16 20:38:56 -0500

    Created SQL procedure to get latest purchases, completed latestPurchases function. Added dummy products, seller, and buyers to the database.

--- SPRX/src/main/java/com/indies/sprx/RestCtrlMain.java
+++ SPRX/src/main/java/com/indies/sprx/RestCtrlMain.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.MediaType;
-import java.util.*;
-
 import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
 
 /*
 *
@@ -25,11 +27,31 @@ public class RestCtrlMain
     JdbcTemplate jdbc_connector; //Connect to MySQL using JDBC.
 
 @RequestMapping(value = "/latestPurchases", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-public String latestPurchases(@RequestParam(required = false) Integer numListRecentPurchases)
+public List<RecentPurchase> latestPurchases(@RequestParam(required = false) Integer numListRecentPurchases, @RequestParam String uname)
 {
     if(numListRecentPurchases == null) {numListRecentPurchases = 2;}
-    jdbc_connector.execute("DROP TABLE IF EXISTS Sometable");
+    
+    List<RecentPurchase> listPurchases = jdbc_connector.query("CALL `getPurchases`(\"" + uname + "\");", new ResultSetExtractor<List>()
+    {
+        public List extractData(ResultSet rs) throws SQLException, DataAccessException
+        {
+            List<RecentPurchase> lp = new ArrayList<RecentPurchase>();
 
-    return "Success!";
+            while(rs.next())
+            {
+                String itemName = rs.getString("Name");
+                String sellerName = rs.getString("FirstName") + " " + rs.getString("LastName");
+                Date datePlaced = rs.getDate("DatePlaced");
+                RecentPurchase newRP = new RecentPurchase(itemName, sellerName, datePlaced);
+                lp.add(newRP);
+            }
+            return lp;
+        }
+    });
+
+
+    if(numListRecentPurchases > listPurchases.size()) {numListRecentPurchases = listPurchases.size();}
+
+    return listPurchases.subList(0, numListRecentPurchases);
 }
 }
