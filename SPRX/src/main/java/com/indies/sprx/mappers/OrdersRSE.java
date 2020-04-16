2ed4282ae3f1209c35604b643612c7f3ac4f459a
AHippoWhoPrograms <AHippoWhoPrograms@github.com>
2020-04-16 19:06:23 +0000

    Started working on Orders ResultSetExtractor.

---
+++ SPRX/src/main/java/com/indies/sprx/mappers/OrdersRSE.java
@@ -0,0 +1,26 @@
+package com.indies.sprx.mappers;
+
+import org.springframework.jdbc.core.ResultSetExtractor;
+import java.sql.ResultSet;
+import com.indies.sprx.Seller;
+import com.indies.sprx.Buyer;
+import com.indies.sprx.User;
+import java.sql.SQLException;
+
+public class OrdersRSE implements ResultSetExtractor<List<OrderPlaced>>
+{
+    @Override
+    public List<OrderPlaced> extraceData(ResultSet rs) throws SQLException
+    {
+        if(rs.next() == false) return null;
+        
+        rs.beforeFirst();
+        List<OrderPlaced> list = new List<>;
+        
+        while(rs.next())
+        {
+            String 
+        }
+
+    }
+}
